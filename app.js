4d0b1ef19d6778b8d331e6e97095a8bbc1ed090e
Pooja <pooja260207@gmail.com>
2026-07-05 16:24:58 +0530

    Fixed port issue

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello Project Working");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server Running on port ${PORT}`);
